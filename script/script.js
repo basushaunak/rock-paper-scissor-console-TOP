ea331174e07683bf2d23ea1bd83d95f182798584
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-04-14 13:05:00 +0530

    Project completed

--- script/script.js
+++ script/script.js
@@ -1,13 +1,14 @@
-/* javascript file for Rock-Paper-Scissors game, console based */
+/* javascript file for Rock-Paper-Scissors game for The Odin Project */
 
-// const choices =["rock","paper","scissors"];
+const choices =["Rock","Paper","Scissors"];
 let playerChoice = -1;
 let computerChoice = -1;
 let playerScore = 0;
 let computerScore = 0;
 let roundCount = 0;
 let totalRounds=0;
-let roundScore = [];
+// let roundScore = [];
+let gameOver = false;
 
 const buttons = document.querySelector(".button-container");
 const rock = document.querySelector("#rock");
@@ -16,14 +17,26 @@ const scissors = document.querySelector("#scissors");
 const btnNextRound = document.querySelector("#next-round");
 const btnComputer = document.querySelector("#computer");
 const btnPlayAgain = document.querySelector("#play-again");
+const divRoundResults = document.querySelector("#round-result");
 const divFinalResult = document.querySelector("#final-result");
-
 
 function getRandomInteger(x, y) {
      return Math.floor(Math.random() * (y - x + 1)) + x;
 }
 
-function init(){
+function initGame(){
+    playerChoice = -1;
+    computerChoice = -1;
+    playerScore = 0;
+    computerScore = 0;
+    roundCount = 0;
+    totalRounds=0;
+    // let roundScore = [];
+    gameOver = false;
+    divFinalResult.innerHTML ="";
+    divRoundResults.innerHTML = "";
+}
+function initRound(){
     playerChoice = -1;
     computerChoice = -1;
     rock.src="../images/rock-white-player.png";
@@ -33,17 +46,37 @@ function init(){
     document.querySelector("#img-paper").src="../images/paper-white-computer.jpg";
     document.querySelector("#img-scissors").src="../images/scissors-white-computer.jpg";
     roundCount++;
-    divFinalResult.innerText = "Round #"+roundCount;
     btnPlayAgain.style.opacity = "0";
     btnNextRound.style.opacity = "0";
     btnComputer.style.opacity = "0"
 }
 
-function updateScore(){
-    divFinalResult.innerText = "Round #" + roundCount + " (Computer: " + computerScore + ", Player: " + playerScore + ")";
+function showResult(winnerId){
+    let htmlResult = divRoundResults.innerHTML;
+    switch (winnerId){
+        case 0:
+            htmlResult += `<p>Its a Tie!</p>`;
+            break;
+        case 1:
+            htmlResult += `<p style="color:green">😎 won, `+ choices[playerChoice] + ` beat ` + choices[computerChoice] + `</p>`;
+            break;
+        case 2:
+            htmlResult += `<p style="color:red">🖳 won, `+ choices[computerChoice] + ` beat ` + choices[playerChoice] + `</p>`;
+            break;
+        default:
+            break;
+    }
+    divRoundResults.innerHTML = htmlResult;
+    if(computerScore === 5 || playerScore === 5){
+        divFinalResult.innerText = "Game Over!Computer: " + computerScore + ", Player: " + playerScore;
+        gameOver = true;
+    } else {
+        divFinalResult.innerText = "Round #" + roundCount + " (Computer: " + computerScore + ", Player: " + playerScore + ")";
+    }
+
 }
 
-function checkResult(){
+function getResult(){
     let winner = 0; /* 0 = Tie, 1= Player, 2 = Computer */
     if(playerChoice === computerChoice){
         return 0;
@@ -78,56 +111,11 @@ function checkResult(){
     }
 }
 
-// function playRound(roundCount){
-//     if((roundCount+1) < totalRounds){
-//     console.log("playing round " + (roundCount+1)+" of " + totalRounds);
-//     } else {
-//         console.log("FINAL ROUND!!!!!");
-//     }
-//     let exitInputLoop = false;
-//     let userInput = "";
-//     while (!exitInputLoop){
-//         userInput = prompt("Please enter rock, paper or scissors").toLowerCase();
-//         if (choices.includes(userInput)){
-//             exitInputLoop=true;
-//         }else {
-//             alert("Please enter rock, paper or scissors only!");
-//         }
-//     }
-//     computerChoice=getRandomInteger(0,2);
-//     playerChoice = choices.indexOf(userInput);
-//     console.log("Computer Choice: " + computerChoice);
-//     console.log("Player Choice: " + playerChoice);
-// }
-
-// function playRockPaperScissors(){
-//     let userInput="";
-//     let exitRoundsLoop = false;
-//     while(!exitRoundsLoop){
-//         userInput = prompt("How many rounds do you want to play?");
-//         totalRounds = Number(userInput);
-//         if(isNaN(totalRounds) || totalRounds<1){
-//             alert("Please enter a valid positive number");
-//             exitRoundsLoop = false;
-//         } else{
-//             exitRoundsLoop = true;
-//         }
-//     }
-//     for (let i=0;i<totalRounds;i++){
-//         playRound(i);
-//     }
-// }
-// playRockPaperScissors();
-
-
-// rock.addEventListener("mouseover",()=>{
-//     rock.src="../images/rock-color-player.png";
-// })
-// rock.addEventListener("mouseout",()=>{
-//     rock.src="../images/rock-white-player.png";
-// })
-
-window.onload = init();
+window.onload = ()=>{
+    initGame();
+    initRound();
+    divFinalResult.innerText = "Round #"+roundCount;
+};
 
 buttons.addEventListener("mouseover",(e)=>{
     if(playerChoice === -1){
@@ -168,39 +156,48 @@ buttons.addEventListener("mouseout",(e)=>{
 })
 
 buttons.addEventListener("click",(e)=>{
+    if (playerChoice === -2){
+        return;
+    }
     let element = e.target.id;
     switch (element){
         case "rock":
             if(playerChoice === 0){
                 playerChoice = -1;
                 rock.src="../images/rock-white-player.png";
+                btnComputer.style.opacity = "0";
             }else {
                 rock.src="../images/rock-color-player.png";
                 paper.src="../images/paper-white-player.png";
                 scissors.src="../images/scissors-white-player.png";
                 playerChoice = 0;
+                btnComputer.style.opacity = "100";
             }
             break;
         case "paper":
             if(playerChoice === 1){
                 playerChoice = -1;
                 paper.src="../images/paper-white-player.png";
+                btnComputer.style.opacity = "0";
             }else {
                 paper.src="../images/paper-color-player.png";
                 rock.src="../images/rock-white-player.png";
                 scissors.src="../images/scissors-white-player.png";
                 playerChoice = 1;
+                btnComputer.style.opacity = "100";
             }
             break;
         case "scissors":
             if(playerChoice === 2){
                 playerChoice = -1;
                 scissors.src="../images/scissors-white-player.png";
+                btnComputer.style.opacity = "0";
             }else {
                 scissors.src="../images/scissors-color-player.png";            
                 rock.src="../images/rock-white-player.png";
                 paper.src="../images/paper-white-player.png";
                 playerChoice = 2;
+                btnComputer.style.opacity = "100";
             }
             break;
         default:
@@ -230,10 +227,32 @@ btnComputer.addEventListener("click",()=>{
         default:
             break;
     }
-
+    let winner = getResult();
+    if (winner === 1){
+        playerScore++;
+    } else if (winner ===2){
+        computerScore++;
+    }
+    showResult(winner);
+    if (!gameOver){
+        btnNextRound.style.opacity = "100";
+        btnComputer.style.opacity = "0";
+        playerChoice = -2;
+        computerChoice = -1;
+    } else{
+        playerChoice = -2;
+        computerChoice = -2;
+        btnPlayAgain.style.opacity="100";
+        btnNextRound.style.opacity = "0";
+        btnComputer.style.opacity = "0";
+    }
 })
 
 btnNextRound.addEventListener("click",()=>{
-    init();
+    initRound();
+})
 
+btnPlayAgain.addEventListener("click",()=>{
+    initGame();
+    initRound();
 })
